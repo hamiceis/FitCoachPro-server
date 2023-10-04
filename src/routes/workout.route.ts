4fb2123384838801056c48b0419d14455faae1d4
hamiceis <hamiceis@hotmail.com>
2023-10-04 01:20:26 -0300

    route updated workout and exercise

--- src/routes/workout.route.ts
+++ src/routes/workout.route.ts
@@ -7,9 +7,12 @@ import { Workout } from "@prisma/client";
 
 import { getStudentWorkouts } from "../utils/get-student-workout";
 import { getTeacherStudentWorkouts } from "../utils/get-student-teacher-workdout";
+import { verificationProperty } from "../utils/verification-data-body";
+
 
 export const routeWorkout = Router();
 
+// Rota verifica se quem se logou foi aluno ou professor, verifica as credenciais e devolve os treinos
 routeWorkout.get("/workouts", async (req: Request, res: Response) => {
   const workouts = await prisma.workout.findMany();
   res.status(200);
@@ -70,7 +73,7 @@ routeWorkout.get(
   }
 );
 
-// Rota para recuperar treinos de aluno
+// Rota para recuperar meus treinos de aluno
 routeWorkout.get(
   "/workouts/student/:studentId",
   authLogin,
@@ -118,113 +121,7 @@ routeWorkout.get(
   }
 );
 
-//Cadastrar um novo treino e exercicio
-routeWorkout.post("/workouts/:studentId", authLogin, async (req: Request, res: Response) => {
-  const authToken = req.cookies.authToken
-  const user: DeserializerUser = JSON.parse(authToken)
-
-  const paramsSchema = z.object({
-    studentId: z.string().uuid()
-  })
-
-  const bodySchema = z.object({
-    type: z.string(),
-    weekDay: z.number(),
-    dayMonth: z.date().optional().default(new Date()),
-    exerciseName: z.string(),
-    repetitions: z.string(),
-    interval: z.string(),
-    method: z.string().optional(),
-    load: z.string(),
-    cadence: z.string(),
-    observation: z.string().optional()
-  })
-
-  const { studentId } = paramsSchema.parse(req.params)
-  const body = bodySchema.parse(req.body);
-
-  const teacher = await prisma.professor.findUnique({
-    where: {
-      id: user.id
-    }
-  })
-
-  const student = await prisma.student.findUnique({
-    where: {
-      id: studentId
-    },
-    include: {
-      professor: true
-    }
-  })
-
-  try {
-    if (!teacher || teacher.id !== user.id) {
-      return res.status(400).json({ message: "Professor não encontrado ou não autorizado"})
-    }
-
-    if (!student) {
-      return res.status(400).json({ message: "Estudante não encontrado"})
-    }
-
-    if (student.professorId !== teacher.id || !student.professor) {
-      return res.status(401).json({ message: "Unauthorized add workout"})
-    }
-
-    // Crie o objeto "data" que contém informações de Workout e Exercise
-    const data: WorkoutAndExerciseProps = {
-      Workout: {
-        id: '',
-        type: body.type,
-        day_month: body.dayMonth || null,
-        week_day: body.weekDay,
-        studentId: student.id
-      },
-      Exercise: {
-        id: '',
-        name_exercise: body.exerciseName,
-        repetitions: body.repetitions,
-        interval: body.interval,
-        method: body.method || null,
-        load: parseFloat(body.load),
-        cadence: body.cadence,
-        observation: body.observation || '',
-        workoutId: ''
-      }
-    }
-
-    // Iniciar uma transação do Prisma
-      const workout = await prisma.workout.create({
-        data: {
-          type: data.Workout.type,
-          week_day: data.Workout.week_day,
-          day_month: data.Workout.day_month,
-          studentId: data.Workout.studentId
-        }
-      })
-
-      const exercise = await prisma.exercise.create({
-        data: {
-          ...data.Exercise,
-          id: workout.id,
-        }
-      })
-
-      const result = {
-        workout,
-        exercise
-      }
-    
-
-    return res.status(200).json(result)
-
-  } catch(error) {
-    console.log("[INTERNAL_SERVER_ERROR_post_workout]", error)
-    return res.status(500).json({ message: "Internal Server Error"})
-  }
-})
-
-// Rota para recuperar treinos dos aluno do professor
+// Rota para recuperar treinos do aluno,  passando o id pelos params
 routeWorkout.get(
   "/workouts/teacher/:studentId",
   authLogin,
@@ -268,3 +165,200 @@ routeWorkout.get(
     }
   }
 );
+
+//Cadastrar um novo treino e exercicio
+routeWorkout.post(
+  "/workouts/:studentId",
+  authLogin,
+  async (req: Request, res: Response) => {
+    const authToken = req.cookies.authToken;
+    const user: DeserializerUser = JSON.parse(authToken);
+
+    const paramsSchema = z.object({
+      studentId: z.string().uuid(),
+    });
+
+    const bodySchema = z.object({
+      type: z.string(),
+      weekDay: z.number(),
+      dayMonth: z.date().optional().default(new Date()),
+      exerciseName: z.string(),
+      repetitions: z.string(),
+      interval: z.string(),
+      method: z.string().optional(),
+      load: z.string(),
+      cadence: z.string(),
+      observation: z.string().optional(),
+    });
+
+    const { studentId } = paramsSchema.parse(req.params);
+    const body = bodySchema.parse(req.body);
+
+    const teacher = await prisma.professor.findUnique({
+      where: {
+        id: user.id,
+      },
+    });
+
+    const student = await prisma.student.findUnique({
+      where: {
+        id: studentId,
+      },
+      include: {
+        professor: true,
+      },
+    });
+
+    try {
+      if (!student) {
+        return res.status(400).json({ message: "Estudante não encontrado" });
+      }
+
+      if (!teacher) {
+        return res
+          .status(400)
+          .json({ message: "Professor não encontrado" });
+      }
+
+      if (student.professorId !== teacher.id || !student.professor) {
+        return res.status(401).json({ message: "Unauthorized add workout" });
+      }
+
+      // Crie o objeto "data" que contém informações de Workout e Exercise
+      const data: WorkoutAndExerciseProps = {
+        Workout: {
+          id: "",
+          type: body.type,
+          day_month: body.dayMonth || null,
+          week_day: body.weekDay,
+          studentId: student.id,
+        },
+        Exercise: {
+          id: "",
+          name_exercise: body.exerciseName,
+          repetitions: body.repetitions,
+          interval: body.interval,
+          method: body.method || null,
+          load: parseFloat(body.load),
+          cadence: body.cadence,
+          observation: body.observation || "",
+          workoutId: "",
+        },
+      };
+
+      const workout = await prisma.workout.create({
+        data: {
+          type: data.Workout.type,
+          week_day: data.Workout.week_day,
+          day_month: data.Workout.day_month,
+          studentId: data.Workout.studentId,
+        },
+      });
+
+      const exercise = await prisma.exercise.create({
+        data: {
+          ...data.Exercise,
+          id: workout.id,
+        },
+      });
+
+      const result = {
+        workout,
+        exercise,
+      };
+
+      return res.status(200).json(result);
+    } catch (error) {
+      console.log("[INTERNAL_SERVER_ERROR_post_workout]", error);
+      return res.status(500).json({ message: "Internal Server Error" });
+    }
+  }
+);
+
+// alterar ou atualiza o treino
+routeWorkout.patch("/workout/:studentId/:exerciseId", authLogin, async (req: Request, res: Response) => {
+  const authToken = req.cookies.authToken
+  const user: DeserializerUser = JSON.parse(authToken)
+
+  const paramsSchema = z.object({
+    studentId: z.string().uuid(),
+    exerciseId: z.string().uuid(),
+  })
+
+  const bodySchema = z.object({
+    type: z.string().optional(),
+    weekDay: z.number().optional(),
+    dayMonth: z.date().optional().default(new Date()),
+    exerciseName: z.string().optional(),
+    repetitions: z.string().optional(),
+    interval: z.string().optional(),
+    method: z.string().optional(),
+    load: z.string().optional(),
+    cadence: z.string().optional(),
+    observation: z.string().optional(),
+  });
+
+  const { studentId, exerciseId } = paramsSchema.parse(req.params)
+  const body = bodySchema.parse(req.body)
+  const data = verificationProperty(body)
+
+  const student = await prisma.student.findUnique({
+    where: {
+      id: studentId
+    },
+    include: {
+      workouts: true,
+    }
+  })
+  const teacher = await prisma.professor.findUnique({
+    where: {
+      id: user.id
+    }
+  })
+
+  const exercise = await prisma.exercise.findUnique({
+    where: {
+      id: exerciseId
+    }
+  })
+ 
+  try {
+
+    if (!student) {
+      return res.status(404).json({ message: "Student not found" });
+    }
+
+    if (!teacher || student.professorId !== user.id) {
+      return res.status(403).json({ message: "Unauthorized" });
+    }
+
+    if (!exercise) {
+      return res.status(404).json({ message: "Exercise not found" });
+    }
+
+
+    const workdout = await prisma.workout.update({
+      where: {
+        id: exercise.id
+      },
+      data: {
+        ...data.workout
+      }
+    })
+
+    await prisma.exercise.update({
+      where: {
+        id: workdout.id
+      },
+      data: {
+        ...data.exercise
+      }
+    })
+
+    return res.status(200).json({ message: "Workout and Exercise updated"})
+    
+  } catch (error) {
+    console.log("[INTERNAL_SERVER_ERROR_patch_workout]", error)
+    return res.status(500).json({ message: "Internal server error"})
+  }
+})
---
+++ src/utils/verification-data-body.ts
@@ -0,0 +1,46 @@
+interface BodyProps {
+  type?: string;
+  weekDay?: number;
+  dayMonth?: Date;
+  exerciseName?: string;
+  interval?: string;
+  repetitions?: string;
+  method?: string;
+  load?: string;
+  cadence?: string;
+  observation?: string;
+}
+
+
+export function verificationProperty(body: BodyProps) {
+  const data = {
+    workout: {} as {
+      type?: string;
+      week_day?: number;
+      day_month?: Date;
+    },
+    exercise: {} as {
+      name_exercise?: string;
+      interval?: string;
+      repetitions?: string;
+      method?: string;
+      load?: number;
+      cadence?: string;
+      observation?: string;
+    },
+  };
+
+  if(body.type) data.workout.type = body.type
+  if(body.weekDay) data.workout.week_day = body.weekDay
+  if(body.dayMonth) data.workout.day_month = body.dayMonth
+
+  if(body.exerciseName) data.exercise.name_exercise = body.exerciseName
+  if(body.repetitions) data.exercise.repetitions = body.repetitions
+  if(body.interval) data.exercise.interval = body.interval
+  if(body.method) data.exercise.method = body.method
+  if(body.cadence) data.exercise.cadence = body.cadence
+  if(body.observation) data.exercise.observation = body.observation
+  if(body.load) data.exercise.load = parseFloat(body.load)
+
+  return data;
+};
